868b03dc52c78c74928f645743fefe85891b813d
Hugo Frota <herbtyh@gmail.com>
2023-05-29 15:14:01 -0300

    Adicionar script de correr e parar

---
+++ scripts/player/player_texture.gd
@@ -0,0 +1,21 @@
+extends Sprite
+class_name PlayerTexture
+
+export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
+
+func animate(direction: Vector2) -> void:
+	verify_position(direction)
+	horizontal_behavior(direction)
+
+func verify_position(direction: Vector2) -> void:
+	if direction.x > 0:
+		flip_h = false
+	elif direction.x < 0:
+		flip_h = true
+
+func horizontal_behavior(direction: Vector2) -> void:
+	if direction.x != 0:
+		animation.play("run")
+	else:
+		animation.play("idle")
+
