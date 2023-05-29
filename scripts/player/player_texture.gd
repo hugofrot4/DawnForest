59e2d7a9d8423ae101b707df72c1b96a43b87c43
Hugo Frota <herbtyh@gmail.com>
2023-05-29 17:43:59 -0300

    Adicionar animações de Ataque, agachar e defender

--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -2,10 +2,19 @@ extends Sprite
 class_name PlayerTexture
 
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
+export(NodePath) onready var player = get_node(player) as KinematicBody2D
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	horizontal_behavior(direction)
+	
+	if direction.y != 0:
+		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("landing")
+		player.set_physics_process(false)
+	else:
+		horizontal_behavior(direction)
+		
 
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
@@ -13,9 +22,21 @@ func verify_position(direction: Vector2) -> void:
 	elif direction.x < 0:
 		flip_h = true
 
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		player.landing = true
+		animation.play("fall")
+	elif direction.y < 0:
+		animation.play("jump")
+
 func horizontal_behavior(direction: Vector2) -> void:
 	if direction.x != 0:
 		animation.play("run")
 	else:
 		animation.play("idle")
 
+func on_animation_finished(anim_name: String):
+	match anim_name:
+		"landing":
+			player.landing = false
+			player.set_physics_process(true)
